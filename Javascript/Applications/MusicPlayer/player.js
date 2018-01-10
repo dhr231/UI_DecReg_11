1f49bba39d7ff3b70f9316760de92eaa78b7fcbc
ravi4all <ravikanttyagi.2295@gmail.com>
2018-01-10 14:03:14 +0530

    Add files via upload

--- Javascript/Applications/MusicPlayer/player.js
+++ Javascript/Applications/MusicPlayer/player.js
@@ -2,20 +2,40 @@ window.addEventListener("load", init);
 
 var audio;
 var slider;
+var starttime;
+var endtime;
 
 function init(){
     audio = document.getElementById("audio");
     slider = document.getElementById("slider");
+    starttime = document.getElementById("starttime");
+    endtime = document.getElementById("endtime");
     document.getElementById("play").addEventListener("click", playSong);
     document.getElementById("pause").addEventListener("click", pauseSong);
     slider.addEventListener("change", forwardSong);
+
+    var songList = document.getElementsByClassName("songName");
+
+    for(var i = 0; i<songList.length; i++){
+        songList[i].addEventListener("click", changeSong);
+    }
+}
+
+function changeSong(){
+    var songname = event.srcElement.innerHTML;
+    console.log(songname);
+    audio.currentTime = 0;
+    audio.src = 'songs/'+songname + '.mp3';
+    // audio.children[0].src = 'songs/'+songname + '.mp3';
+    // playSong();
+    setInterval(playSong, 1000);
 }
 
 function playSong(){
     audio.play();
-    setInterval(function(){
-        slider.value = audio.currentTime;
-    }, 1000);
+    slider.value = audio.currentTime;
+    starttime.innerHTML = parseInt(audio.currentTime);
+    endtime.innerHTML = audio.duration;
 }
 
 function pauseSong(){
